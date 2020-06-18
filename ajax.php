b1b6ecd136afc343a341a4913965ad635e876a8f
miluge <gllmblndl@gmail.com>
2020-06-18 19:10:09 +0200

    tada

--- ajax.php
+++ ajax.php
@@ -5,11 +5,11 @@ if (!(in_array($_FILES['file']['type'], $arr_file_types))) {
     echo "Type de fichier acceptés : png / gif / txt / jpeg / jpg / pdf / rar";
     return;
 }
- 
+
 if (!file_exists('accueil')) {
-    mkdir('accueil', 0777);
+   mkdir('accueil', 0777);
 }
- 
+
 move_uploaded_file($_FILES['file']['tmp_name'], 'accueil/' . time() . '_' . $_FILES['file']['name']);
  
 echo "GREAT SUCCESS!";
